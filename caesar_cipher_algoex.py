cf8abf5ff83a538c5d6a4cc838c54f6d073368ab
Deepankar <dip2leo@gmail.com>
2022-07-03 14:02:16 -0400

    added caesar

--- caesar_cipher_algoex.py
+++ caesar_cipher_algoex.py
@@ -13,5 +13,19 @@ def caesarCipherEncryptor(string, key):
     
     return answer 
 
+def caesarCipherEncryptor2(string,key):
+    answer = ""
+
+    for char in string:
+        letter = ord(char) + (key%26)
+
+        if(letter <= 122):
+            answer+= chr(letter)
+        else:
+            answer+= chr(96+letter % 122)
+
+    return answer
+
 string = "kjwmntauvjjnmsagwgawkagfuaugjhawgnawgjhawjgawbfawghesh"
-print(caesarCipherEncryptor(string,15))
+print(caesarCipherEncryptor(string,55))
+print(caesarCipherEncryptor2(string,55))
