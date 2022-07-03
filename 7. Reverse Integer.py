ee51a847f5fad026d7088cd424b4b520c42ab39e
Deepankar <dip2leo@gmail.com>
2022-07-03 14:57:15 -0400

    added 7_reverse_int

---
+++ 7. Reverse Integer.py
@@ -0,0 +1,44 @@
+#https://leetcode.com/problems/reverse-integer/
+
+def sol(x):
+    y = 0
+    sin = False
+    if x < 0:
+        sin = True
+        x = abs(x)
+
+    while(abs(x) > 0):
+        y = (y*10) + (x % 10)
+        x = x//10
+        if(abs(y) > 2**31):
+            return 0 
+
+    return y if not sin else -y
+
+def sol2(x):
+    pass 
+
+x = 123
+
+print(sol(x))
+
+
+'''
+JAVA
+
+public int reverse(int x){
+    boolean sign = x > 0; 
+ 
+    x = Math.abs(x); 
+
+    int resule = 0; 
+
+    while(x > 0){
+        if(result > Integer.MAX_VALUE/10) return 0; 
+        result = (result * 10) + (x % 10); 
+        x = x/10 ; 
+    }
+
+    return sign ? result: -result
+}
+'''
