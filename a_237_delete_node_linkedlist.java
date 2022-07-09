910745d269bd41f3deaa14b209a608ad11e1b387
Deepankar <dip2leo@gmail.com>
2022-07-09 13:02:38 -0400

    added 237

--- a_237_delete_node_linkedlist.java
+++ a_237_delete_node_linkedlist.java
@@ -1,5 +1,21 @@
 public class a_237_delete_node_linkedlist {
+    public class ListNode {
+            int val;
+            ListNode next;
+            ListNode(int x) {
+                val = x; 
+            }
+    }
+    
     public static void main(String[] args) {
         
+    }
+
+    public static void deleteNode(ListNode node) {
+        
+       /*while(node.val != null && node.next != null){
+            if(node.val == val)
+        }
+       */ 
     }
 }
