33023a58ccdd25f84fafbfafaec5d2987fb494f5
Deepankar <dip2leo@gmail.com>
2022-08-27 10:27:23 -0400

    added arrayList

---
+++ minimum_waiting_time.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class minimum_waiting_time {
+    public static void main(String[] args) {
+        ArrayList<Integer> newList = new ArrayList<>(Arrays.asList(3,4,5,6,2));
+
+        System.out.println(newList);
+
+    }
+}
---
+++ minimun_waiting_time_algo_ex.py
@@ -0,0 +1,5 @@
+def minimumWaitingTime(queries):
+    # Write your code here.
+    return 0
+
+print(minimumWaitingTime[3,2,1,2,6])
